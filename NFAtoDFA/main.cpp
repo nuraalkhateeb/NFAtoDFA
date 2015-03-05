4b6cc1c12f4c8e62dac44f8de44a44c2697da3bd
nuraalkhateeb <nuraalkhateeb@gmail.com>
2015-03-05 12:18:14 -0500

    Finished processing input file

--- NFAtoDFA/main.cpp
+++ NFAtoDFA/main.cpp
@@ -1,21 +1,20 @@
-//
-//  main.cpp
-//  NFAtoDFA
-//
-//  Created by Apple iMac on 3/4/15.
-//  Copyright (c) 2015 Apple iMac. All rights reserved.
-//
+//NFA to DFA program
 
 #include <iostream>
 #include <stdlib.h>
 #include <stdio.h>
 #include <fstream>
 #include <vector>
+#include <list>
+#include <tuple>
 
 using namespace std;
 
 int main(){
-    ifstream inputFile("/Users/iMac/Documents/Spring2015/Automata/inputFile.txt");
+    ifstream inputFile("./inputFile.txt");
+	if(!inputFile.is_open()) {
+		cout << "Error opening file";
+	}
     vector<char> file;
     char character;
     int numStates;
@@ -23,8 +22,10 @@ int main(){
     vector<int> acceptingStates;
     int numAcceptingStates;
     int initialState;
-    //transitionss
+    //transitions
+	vector<tuple<int, char, int>> transitions;
     int lineNumber = 1;
+
     do{
         inputFile.get(character);
         file.push_back(character);
@@ -49,15 +50,29 @@ int main(){
     }
     cout << endl;
     numAcceptingStates = (file[i] - 48);                // number of accepting states
-    i++;
+    i = i + 2;
     while(file[i] != '\n'){
+		if(file[i] == 0x20){
+			i++;
+			continue;
+		}
         acceptingStates.push_back((file[i] - 48));      // vector of accepting states
         if(file[i] == '\n')
             break;
         i++;
     }
+	i++;
     for(int z = 0; z < acceptingStates.size(); z++){
         cout << acceptingStates[z];
     }
     cout << endl;
+	initialState = (file[i] - 48);						// initial state number
+	i = i + 2;
+	while(i < file.size()){
+		tuple<int, char, int> transition ((file[i] - 48), file[i + 2], (file[i + 4] - 48));
+		i = i + 6;
+		transitions.push_back(transition);
+		if(i >= file.size())
+			break;
+	}
 }
